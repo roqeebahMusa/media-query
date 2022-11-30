6e2b9c675c18578c35c92994213c69147eb0e35b
roqeebahMusa <roqeebahmusa19@gmail.com>
2022-11-30 10:21:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+// import Header from './Components/Header/Header'
+import Facebook from './Components/Facebook'
+
+
+function App () {
+  return (
+    <div>
+      {/* <Header/> */}
+      <Facebook/>
+    </div>
+  )
+}
+
+export default App
